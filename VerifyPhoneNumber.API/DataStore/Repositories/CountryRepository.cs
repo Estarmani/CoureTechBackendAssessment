e57cb24350ed09fe955b56fa4a7a446556e61db6
Estarmani <zamaniesther@gmail.com>
2024-11-13 12:02:37 +0100

    Updated Country repository

--- VerifyPhoneNumber.API/DataStore/Repositories/CountryRepository.cs
+++ VerifyPhoneNumber.API/DataStore/Repositories/CountryRepository.cs
@@ -14,27 +14,44 @@ namespace VerifyPhoneNumber.API.DataStore.Repositories
         {
             _countries = new List<Country>
             {
-            new Country
-            {
-                CountryId = 1, CountryCode = "234", Name = "Nigeria", CountryIso = "NG",
-                CountryDetails = new List<CountryDetail>
+                new Country
                 {
-                    new CountryDetail { CountryDetailId = 1, CountryId = 1, Operator = "MTN Nigeria", OperatorCode = "MTN NG" },
-                    new CountryDetail { CountryDetailId = 2, CountryId = 1, Operator = "Airtel Nigeria", OperatorCode = "ANG" },
-                    new CountryDetail { CountryDetailId = 3, CountryId = 1, Operator = "9Mobile Nigeria", OperatorCode = "ETN" },
-                    new CountryDetail { CountryDetailId = 4, CountryId = 1, Operator = "Globacom Nigeria", OperatorCode = "GLO NG" }
-                }
-            },
-            new Country
-            {
-                CountryId = 2, CountryCode = "233", Name = "Ghana", CountryIso = "GH",
-                CountryDetails = new List<CountryDetail>
+                    CountryId = 1, CountryCode = "234", Name = "Nigeria", CountryIso = "NG",
+                    CountryDetails = new List<CountryDetail>
+                    {
+                        new CountryDetail { CountryDetailId = 1, CountryId = 1, Operator = "MTN Nigeria", OperatorCode = "MTN NG" },
+                        new CountryDetail { CountryDetailId = 2, CountryId = 1, Operator = "Airtel Nigeria", OperatorCode = "ANG" },
+                        new CountryDetail { CountryDetailId = 3, CountryId = 1, Operator = "9Mobile Nigeria", OperatorCode = "ETN" },
+                        new CountryDetail { CountryDetailId = 4, CountryId = 1, Operator = "Globacom Nigeria", OperatorCode = "GLO NG" }
+                    }
+                },
+                new Country
                 {
-                    new CountryDetail { CountryDetailId = 5, CountryId = 2, Operator = "Vodafone Ghana", OperatorCode = "Vodafone GH" },
-                    new CountryDetail { CountryDetailId = 6, CountryId = 2, Operator = "MTN Ghana", OperatorCode = "MTN Ghana" },
-                    new CountryDetail { CountryDetailId = 7, CountryId = 2, Operator = "Tigo Ghana", OperatorCode = "Tigo Ghana" }
+                    CountryId = 2, CountryCode = "233", Name = "Ghana", CountryIso = "GH",
+                    CountryDetails = new List<CountryDetail>
+                    {
+                        new CountryDetail { CountryDetailId = 5, CountryId = 2, Operator = "Vodafone Ghana", OperatorCode = "Vodafone GH" },
+                        new CountryDetail { CountryDetailId = 6, CountryId = 2, Operator = "MTN Ghana", OperatorCode = "MTN Ghana" },
+                        new CountryDetail { CountryDetailId = 7, CountryId = 2, Operator = "Tigo Ghana", OperatorCode = "Tigo Ghana" }
+                    }
+                },
+                new Country
+                {
+                    CountryId = 3, CountryCode = "229", Name = "Benin Republic", CountryIso = "BN",
+                    CountryDetails = new List<CountryDetail>
+                    {
+                        new CountryDetail { CountryDetailId = 8, CountryId = 3, Operator = "MTN Benin", OperatorCode = "MTN Benin" },
+                        new CountryDetail { CountryDetailId = 9, CountryId = 3, Operator = "Moov Benin", OperatorCode = "Moov Benin" }
+                    }
+                },
+                new Country
+                {
+                    CountryId = 4, CountryCode = "225", Name = "Côte d'Ivoire", CountryIso = "CIV",
+                    CountryDetails = new List<CountryDetail>
+                    {
+                        new CountryDetail { CountryDetailId = 10, CountryId = 4, Operator = "MTN Côte d'Ivoire", OperatorCode = "MTN CIV" }
+                    }
                 }
-            }
             };
 
             _context = context;
